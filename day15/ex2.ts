cdc2f5e80ffc6a65d299d8f7a8cbc9eb381b285c
Colin Dembovsky <colindembovsky@github.com>
2021-12-16 16:17:59 +0000

    Complete 15.2

--- day15/ex2.ts
+++ day15/ex2.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 
-const tile = fs.readFileSync('inputs/input15-test.txt', 'utf8').split('\n').map(line => line.split('').map(Number));
+const tile = fs.readFileSync('inputs/input15.txt', 'utf8').split('\n').map(line => line.split('').map(Number));
 
 let WIDTH = tile[0].length;
 let HEIGHT = tile.length;
@@ -20,36 +20,56 @@ class Node {
     constructor(public distance: number, public r: number, public c: number) { }
 }
 
-// dijkstra
-const D = new Array(HEIGHT * 5).fill(0).map(() => new Array(WIDTH * 5).fill(-1));
-const Q = [new Node(0, 0, 0)];
-
+// convenience
 let NXROW = [-1, 0, 1,  0];
 let NXCOL = [ 0, 1, 0, -1];
 
-while(Q.length > 0) {
-    const curNode = Q.pop()!;
-    const [dist, r, c] = [curNode.distance, curNode.r, curNode.c];
+// dijkstra
+function solve(tileCount: number) {
+    const D = new Array(HEIGHT * tileCount).fill(0).map(() => new Array(WIDTH * tileCount).fill(undefined));
+    const Q = [new Node(0, 0, 0)];
+    
+    let iter = 0;
+    while(Q.length > 0) {
+        iter++;
 
-    if (r < 0 || c < 0 || r >= HEIGHT * 5 || c >= WIDTH * 5) {
-        continue;
+        // pop the smallest distance node off the Q
+        let curNode = Q[0];
+        for(let i = 1; i < Q.length; i++) {
+            if(Q[i].distance < curNode.distance) {
+                curNode = Q[i];
+            }
+        }
+        Q.splice(Q.indexOf(curNode), 1);
+        
+        const [dist, r, c] = [curNode.distance, curNode.r, curNode.c];
+
+        if (r < 0 || c < 0 || r >= HEIGHT * tileCount || c >= WIDTH * tileCount) {
+            continue;
+        }
+        const cost = dist + getVal(r, c);
+
+        if (D[r][c] === undefined || cost < D[r][c]) {
+            D[r][c] = cost;
+        } else {
+            continue;
+        }
+
+        //console.log(`[${iter}] ${r} ${c} ${dist} ${cost}`);
+        if (r === (HEIGHT * tileCount) - 1 && c === (WIDTH * tileCount) - 1) {
+            break;
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const nextRow = r + NXROW[i];
+            const nextCol = c + NXCOL[i];
+            Q.push(new Node(D[r][c], nextRow, nextCol));
+        }
+
     }
-    const val = getVal(r, c);
-
-    if (D[r][c] === -1 || curNode.distance + val < D[r][c]) {
-        D[r][c] = dist + val;
-    } else {
-        continue;
-    }
-
-    console.log(`${r} ${c} ${val} ${dist}`);
-    if (r === (HEIGHT * 5) - 1 && c === (WIDTH * 5) - 1) break;
-
-    for (let i = 0; i < 4; i++) {
-        const nextRow = r + NXROW[i];
-        const nextCol = c + NXCOL[i];
-        Q.push(new Node(D[r][c], nextRow, nextCol));
-    }
+    console.log(`${iter} iterations`);
+    return D[(HEIGHT * tileCount) - 1][(WIDTH * tileCount) - 1] - tile[0][0]
 }
 
-console.log(`Result: ${D[(HEIGHT * 5) - 1][(WIDTH * 5) - 1] - tile[0][0]}`);
+console.log(solve(1));
+console.log(solve(5));
