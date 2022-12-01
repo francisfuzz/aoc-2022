04c5f28109b80ff9be7cea2de59263e9f533a83f
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-12-01 18:31:20 +0000

    fix: remove unused file

--- day1/day1-ex2.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-
-// read in a file and print it out to the console
-function readFile(fileName: string): string {
-    return fs.readFileSync(fileName, "utf8");
-}
-
-let contents = readFile("inputs/input1.txt");
-console.log(contents);
