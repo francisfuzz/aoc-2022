f4059cc541055f244e5385658087d55a645eca54
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-12-03 19:12:35 +0000

    feat: use reduce and annotate the file

--- day3/day3-p1.ts
+++ day3/day3-p1.ts
@@ -3,31 +3,35 @@ import {readFileSync} from "fs";
 const contents = readFileSync("inputs/input3.txt", "utf8");
 const rucksackData: Array<string> = contents.split("\n");
 
+// Map a letter to its priority score.
 type AlphabetScorer = {[letter: string]: number};
-
 const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const alphabetScorer: AlphabetScorer = (alphabet).split('').reduce((acc: AlphabetScorer, letter: string, index: number) => {
   acc[letter] = index + 1;
   return acc;
 }, {});
 
-let totalScore = 0
-
-rucksackData.forEach((rucksack: string) => {
+// Calculate the total priority score given the rucksack data.
+const totalScore = rucksackData.reduce((accumulator: number, rucksack: string): number => {
+  // Parse the rucksack into its compartments.
   const demarcation = rucksack.length / 2;
   const firstCompartment = rucksack.slice(0, demarcation);
   const secondCompartment = rucksack.slice(demarcation);
 
-  // Create a new set from the first compartment
+  // Create a new set from the first compartment.
   const s = new Set([...firstCompartment.split('')]);
 
-  // Return as soon as the matching letter is found.
+  // Iterate through the second compartment's elements until there's a match.
   for (let i = 0; i < secondCompartment.length; i++) {
-    if(s.has(secondCompartment[i])) {
-      totalScore += alphabetScorer[secondCompartment[i]];
-      return;
+    if (s.has(secondCompartment[i])) {
+      // Return as soon as the matching letter is found,
+      // adding the value to the accumulator.
+      return accumulator + alphabetScorer[secondCompartment[i]];
     }
   }
-});
+
+  // If no match is found from the earlier loop, return the accumulator.
+  return accumulator;
+}, 0);
 
 console.log(totalScore);
