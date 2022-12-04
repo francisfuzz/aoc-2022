3e7e8fad37518b1b474eacb656efc1b9ec504168
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-12-04 05:57:14 +0000

    feat: add soln to meet sample

--- day4/day4-p1.ts
+++ day4/day4-p1.ts
@@ -5,4 +5,26 @@ const data: Array<string> = contents.split("\n");
 
 // Part 1 Prompt
 // In how many assignment pairs does one range fully contain the other?
-console.log(data);
+
+// Determines if `n` is inclusively between the minimum bound and maximum bound specified.
+function isWithin (n: number, minBound: number, maxBound: number): boolean {
+  return n >= minBound && n <= maxBound
+}
+
+let containmentPairs = 0;
+
+// Think about `2-4,6-8` as an example
+data.forEach((sectionAssignmentPair: string) => {
+  // Split the section assignments.
+  const [first, second] = sectionAssignmentPair.split(',');
+
+  // Parse the numbers in each assignment.
+  const [a1, a2] = first.split('-').map(i => parseInt(i));
+  const [b1, b2] = second.split('-').map(i => parseInt(i));
+
+  if ((isWithin(a1, b1, b2) && isWithin(a2, b1, b2)) || (isWithin(b1, a1, a2) && isWithin(b2, a1, a2))) {
+      containmentPairs += 1;
+    }
+})
+
+console.log(containmentPairs);
