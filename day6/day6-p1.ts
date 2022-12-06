9c5cd544d8d3c02def259e7c9ad0b483a2123a7a
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-12-06 13:59:22 +0000

    feat: add day 6 p1

---
+++ day6/day6-p1.ts
@@ -0,0 +1,24 @@
+// Setup.
+import {readFileSync} from "fs";
+const contents = readFileSync("inputs/input6.txt", "utf8");
+const data: string[] = contents.split("\n");
+
+const markers: number[] = [];
+
+for (let i = 0; i < data.length; i++) {
+  const buffer = data[i];
+
+  for (let j = 4; j < buffer.length; j++) {
+    const chunk = buffer.slice(j - 4, j);
+    const uniqueCharactersInChunk = new Set(chunk);
+
+    if (uniqueCharactersInChunk.size === 4) {
+      markers.push(j);
+      break;
+    }
+  }
+}
+
+const processedCharacters = markers.reduce((p: number, c: number): number => { return p + c }, 0);
+
+console.log(`Processed characters: ${processedCharacters}`);
