8231b966988a5ad2b081160ca4b9bd3ec6df253a
Francis <15894826+francisfuzz@users.noreply.github.com>
2022-12-06 13:57:01 +0000

    feat: add day 6 sample

--- day6/day6-sample.ts
+++ day6/day6-sample.ts
@@ -3,4 +3,22 @@ import {readFileSync} from "fs";
 const contents = readFileSync("inputs/input6-sample.txt", "utf8");
 const data: string[] = contents.split("\n");
 
-console.log(data);
+const markers: number[] = [];
+
+for (let i = 0; i < data.length; i++) {
+  const buffer = data[i];
+
+  for (let j = 4; j < buffer.length; j++) {
+    const chunk = buffer.slice(j - 4, j);
+    const uniqueCharactersInChunk = new Set(chunk);
+
+    if (uniqueCharactersInChunk.size === 4) {
+      markers.push(j);
+      break;
+    }
+  }
+}
+
+const processedCharacters = markers.reduce((p: number, c: number): number => { return p + c }, 0);
+
+console.log(`Processed characters: ${processedCharacters}`);
